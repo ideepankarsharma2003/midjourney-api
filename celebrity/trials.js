bc1a6f8dc17a2eac11e28bf79d788e845e1aac83
deepankar <deepankar@warewe.com>
2024-03-21 15:07:15 +0530

    setup midjourney api

--- celebrity/trials.js
+++ celebrity/trials.js
@@ -18,7 +18,7 @@ async function main() {
     SessionId: process.env.SALAI_TOKEN || "8bb7f5b79c7a49f7d0824ab4b8773a81",
   });
   await client.init();
-  const msg = await client.Imagine("A little pink elephant", (uri) => {
+  const msg = await client.Imagine('https://image.tmdb.org/t/p/original/yG8eiRj5M7JL4l8hmYQUGNRcpOG.jpg https://image.tmdb.org/t/p/original/eogRp6oAPK0SEvQmCrQ78LTlSdp.jpg https://image.tmdb.org/t/p/original/lU1ollblNqLSAof7fzvfiHSqb9C.jpg tom cruise in dress of ben 10 "mission immpossible" written on a banner in background  --iw 2 --v 6', (uri) => {
     console.log("loading", uri);
   });
   console.log({ msg });
@@ -27,3 +27,6 @@ main().catch((err) => {
   console.error(err);
   process.exit(1);
 });
+
+
+
